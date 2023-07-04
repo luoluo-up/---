74b42b29a45f4c1607868044d89b153af44ba238
luoluo <601119280@qq.com>
2023-07-05 00:11:25 +0800

    http改https

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://syt.atguigu.cn',
+        target: 'https://syt.atguigu.cn',
         changeOrigin: true,
       },
     }
